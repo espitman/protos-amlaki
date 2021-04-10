80445fa16a27050d2bb8a2dc36ae09b5962a6fc4
Saeed Heidari <saeed.heidari86@gmail.com>
2021-04-10 20:33:26 +0430

    house update

--- house/house.methoods.go
+++ house/house.methoods.go
@@ -15,3 +15,10 @@ func (t *RequestDetails) Unmarshal(entry interface{}) RequestDetails {
 	_ = json.Unmarshal(obj, &response)
 	return response
 }
+
+func (t *RequestCreate) Unmarshal(entry interface{}) RequestCreate {
+	obj, _ := json.Marshal(entry)
+	response := RequestCreate{}
+	_ = json.Unmarshal(obj, &response)
+	return response
+}
