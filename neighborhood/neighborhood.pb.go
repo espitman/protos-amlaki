362ceaae7d05df0a9e425278a28797862b31a9eb
Saeed Heidari <saeed.heidari86@gmail.com>
2021-04-16 12:57:48 +0430

    neighborhood update

--- neighborhood/neighborhood.pb.go
+++ neighborhood/neighborhood.pb.go
@@ -272,6 +272,100 @@ func (x *RequestCreate) GetCreator() string {
 	return ""
 }
 
+type RequestCheckExistByProvinceIdAndCityId struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Neighborhood *Neighborhood `protobuf:"bytes,1,opt,name=neighborhood,proto3" json:"neighborhood,omitempty"`
+}
+
+func (x *RequestCheckExistByProvinceIdAndCityId) Reset() {
+	*x = RequestCheckExistByProvinceIdAndCityId{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_neighborhood_neighborhood_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *RequestCheckExistByProvinceIdAndCityId) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RequestCheckExistByProvinceIdAndCityId) ProtoMessage() {}
+
+func (x *RequestCheckExistByProvinceIdAndCityId) ProtoReflect() protoreflect.Message {
+	mi := &file_neighborhood_neighborhood_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RequestCheckExistByProvinceIdAndCityId.ProtoReflect.Descriptor instead.
+func (*RequestCheckExistByProvinceIdAndCityId) Descriptor() ([]byte, []int) {
+	return file_neighborhood_neighborhood_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *RequestCheckExistByProvinceIdAndCityId) GetNeighborhood() *Neighborhood {
+	if x != nil {
+		return x.Neighborhood
+	}
+	return nil
+}
+
+type ResponseCheckExistByProvinceIdAndCityId struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	IsExist bool `protobuf:"varint,1,opt,name=isExist,proto3" json:"isExist,omitempty"`
+}
+
+func (x *ResponseCheckExistByProvinceIdAndCityId) Reset() {
+	*x = ResponseCheckExistByProvinceIdAndCityId{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_neighborhood_neighborhood_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ResponseCheckExistByProvinceIdAndCityId) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ResponseCheckExistByProvinceIdAndCityId) ProtoMessage() {}
+
+func (x *ResponseCheckExistByProvinceIdAndCityId) ProtoReflect() protoreflect.Message {
+	mi := &file_neighborhood_neighborhood_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ResponseCheckExistByProvinceIdAndCityId.ProtoReflect.Descriptor instead.
+func (*ResponseCheckExistByProvinceIdAndCityId) Descriptor() ([]byte, []int) {
+	return file_neighborhood_neighborhood_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *ResponseCheckExistByProvinceIdAndCityId) GetIsExist() bool {
+	if x != nil {
+		return x.IsExist
+	}
+	return false
+}
+
 var File_neighborhood_neighborhood_proto protoreflect.FileDescriptor
 
 var file_neighborhood_neighborhood_proto_rawDesc = []byte{
@@ -301,13 +395,33 @@ var file_neighborhood_neighborhood_proto_rawDesc = []byte{
 	0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x52, 0x0c, 0x6e, 0x65, 0x69,
 	0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x63, 0x72, 0x65,
 	0x61, 0x74, 0x6f, 0x72, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x63, 0x72, 0x65, 0x61,
-	0x74, 0x6f, 0x72, 0x32, 0x5d, 0x0a, 0x13, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68,
+	0x74, 0x6f, 0x72, 0x22, 0x68, 0x0a, 0x26, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x43, 0x68,
+	0x65, 0x63, 0x6b, 0x45, 0x78, 0x69, 0x73, 0x74, 0x42, 0x79, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x6e,
+	0x63, 0x65, 0x49, 0x64, 0x41, 0x6e, 0x64, 0x43, 0x69, 0x74, 0x79, 0x49, 0x64, 0x12, 0x3e, 0x0a,
+	0x0c, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f,
+	0x6f, 0x64, 0x2e, 0x4e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x52,
+	0x0c, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x22, 0x43, 0x0a,
+	0x27, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x45, 0x78,
+	0x69, 0x73, 0x74, 0x42, 0x79, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x6e, 0x63, 0x65, 0x49, 0x64, 0x41,
+	0x6e, 0x64, 0x43, 0x69, 0x74, 0x79, 0x49, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x69, 0x73, 0x45, 0x78,
+	0x69, 0x73, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x69, 0x73, 0x45, 0x78, 0x69,
+	0x73, 0x74, 0x32, 0xf0, 0x01, 0x0a, 0x13, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68,
 	0x6f, 0x6f, 0x64, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x46, 0x0a, 0x06, 0x43, 0x72,
 	0x65, 0x61, 0x74, 0x65, 0x12, 0x1b, 0x2e, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68,
 	0x6f, 0x6f, 0x64, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x43, 0x72, 0x65, 0x61, 0x74,
 	0x65, 0x1a, 0x1d, 0x2e, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64,
 	0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73,
-	0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x00, 0x12, 0x90, 0x01, 0x0a, 0x1f, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x45, 0x78, 0x69, 0x73,
+	0x74, 0x42, 0x79, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x6e, 0x63, 0x65, 0x49, 0x64, 0x41, 0x6e, 0x64,
+	0x43, 0x69, 0x74, 0x79, 0x49, 0x64, 0x12, 0x34, 0x2e, 0x6e, 0x65, 0x69, 0x67, 0x68, 0x62, 0x6f,
+	0x72, 0x68, 0x6f, 0x6f, 0x64, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x43, 0x68, 0x65,
+	0x63, 0x6b, 0x45, 0x78, 0x69, 0x73, 0x74, 0x42, 0x79, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x6e, 0x63,
+	0x65, 0x49, 0x64, 0x41, 0x6e, 0x64, 0x43, 0x69, 0x74, 0x79, 0x49, 0x64, 0x1a, 0x35, 0x2e, 0x6e,
+	0x65, 0x69, 0x67, 0x68, 0x62, 0x6f, 0x72, 0x68, 0x6f, 0x6f, 0x64, 0x2e, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x45, 0x78, 0x69, 0x73, 0x74, 0x42, 0x79,
+	0x50, 0x72, 0x6f, 0x76, 0x69, 0x6e, 0x63, 0x65, 0x49, 0x64, 0x41, 0x6e, 0x64, 0x43, 0x69, 0x74,
+	0x79, 0x49, 0x64, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -322,23 +436,28 @@ func file_neighborhood_neighborhood_proto_rawDescGZIP() []byte {
 	return file_neighborhood_neighborhood_proto_rawDescData
 }
 
-var file_neighborhood_neighborhood_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_neighborhood_neighborhood_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_neighborhood_neighborhood_proto_goTypes = []interface{}{
-	(*Neighborhood)(nil),    // 0: neighborhood.Neighborhood
-	(*RequestDetails)(nil),  // 1: neighborhood.RequestDetails
-	(*ResponseDetails)(nil), // 2: neighborhood.ResponseDetails
-	(*RequestCreate)(nil),   // 3: neighborhood.RequestCreate
+	(*Neighborhood)(nil),                            // 0: neighborhood.Neighborhood
+	(*RequestDetails)(nil),                          // 1: neighborhood.RequestDetails
+	(*ResponseDetails)(nil),                         // 2: neighborhood.ResponseDetails
+	(*RequestCreate)(nil),                           // 3: neighborhood.RequestCreate
+	(*RequestCheckExistByProvinceIdAndCityId)(nil),  // 4: neighborhood.RequestCheckExistByProvinceIdAndCityId
+	(*ResponseCheckExistByProvinceIdAndCityId)(nil), // 5: neighborhood.ResponseCheckExistByProvinceIdAndCityId
 }
 var file_neighborhood_neighborhood_proto_depIdxs = []int32{
 	0, // 0: neighborhood.ResponseDetails.neighborhood:type_name -> neighborhood.Neighborhood
 	0, // 1: neighborhood.RequestCreate.neighborhood:type_name -> neighborhood.Neighborhood
-	3, // 2: neighborhood.neighborhoodService.Create:input_type -> neighborhood.RequestCreate
-	2, // 3: neighborhood.neighborhoodService.Create:output_type -> neighborhood.ResponseDetails
-	3, // [3:4] is the sub-list for method output_type
-	2, // [2:3] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	0, // 2: neighborhood.RequestCheckExistByProvinceIdAndCityId.neighborhood:type_name -> neighborhood.Neighborhood
+	3, // 3: neighborhood.neighborhoodService.Create:input_type -> neighborhood.RequestCreate
+	4, // 4: neighborhood.neighborhoodService.CheckExistByProvinceIdAndCityId:input_type -> neighborhood.RequestCheckExistByProvinceIdAndCityId
+	2, // 5: neighborhood.neighborhoodService.Create:output_type -> neighborhood.ResponseDetails
+	5, // 6: neighborhood.neighborhoodService.CheckExistByProvinceIdAndCityId:output_type -> neighborhood.ResponseCheckExistByProvinceIdAndCityId
+	5, // [5:7] is the sub-list for method output_type
+	3, // [3:5] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_neighborhood_neighborhood_proto_init() }
@@ -395,6 +514,30 @@ func file_neighborhood_neighborhood_proto_init() {
 				return nil
 			}
 		}
+		file_neighborhood_neighborhood_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RequestCheckExistByProvinceIdAndCityId); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_neighborhood_neighborhood_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ResponseCheckExistByProvinceIdAndCityId); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -402,7 +545,7 @@ func file_neighborhood_neighborhood_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_neighborhood_neighborhood_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -429,6 +572,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type NeighborhoodServiceClient interface {
 	Create(ctx context.Context, in *RequestCreate, opts ...grpc.CallOption) (*ResponseDetails, error)
+	CheckExistByProvinceIdAndCityId(ctx context.Context, in *RequestCheckExistByProvinceIdAndCityId, opts ...grpc.CallOption) (*ResponseCheckExistByProvinceIdAndCityId, error)
 }
 
 type neighborhoodServiceClient struct {
@@ -448,9 +592,19 @@ func (c *neighborhoodServiceClient) Create(ctx context.Context, in *RequestCreat
 	return out, nil
 }
 
+func (c *neighborhoodServiceClient) CheckExistByProvinceIdAndCityId(ctx context.Context, in *RequestCheckExistByProvinceIdAndCityId, opts ...grpc.CallOption) (*ResponseCheckExistByProvinceIdAndCityId, error) {
+	out := new(ResponseCheckExistByProvinceIdAndCityId)
+	err := c.cc.Invoke(ctx, "/neighborhood.neighborhoodService/CheckExistByProvinceIdAndCityId", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // NeighborhoodServiceServer is the server API for NeighborhoodService service.
 type NeighborhoodServiceServer interface {
 	Create(context.Context, *RequestCreate) (*ResponseDetails, error)
+	CheckExistByProvinceIdAndCityId(context.Context, *RequestCheckExistByProvinceIdAndCityId) (*ResponseCheckExistByProvinceIdAndCityId, error)
 }
 
 // UnimplementedNeighborhoodServiceServer can be embedded to have forward compatible implementations.
@@ -459,6 +613,9 @@ type UnimplementedNeighborhoodServiceServer struct {
 
 func (*UnimplementedNeighborhoodServiceServer) Create(context.Context, *RequestCreate) (*ResponseDetails, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (*UnimplementedNeighborhoodServiceServer) CheckExistByProvinceIdAndCityId(context.Context, *RequestCheckExistByProvinceIdAndCityId) (*ResponseCheckExistByProvinceIdAndCityId, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckExistByProvinceIdAndCityId not implemented")
 }
 
 func RegisterNeighborhoodServiceServer(s *grpc.Server, srv NeighborhoodServiceServer) {
@@ -483,6 +640,24 @@ func _NeighborhoodService_Create_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _NeighborhoodService_CheckExistByProvinceIdAndCityId_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RequestCheckExistByProvinceIdAndCityId)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(NeighborhoodServiceServer).CheckExistByProvinceIdAndCityId(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/neighborhood.neighborhoodService/CheckExistByProvinceIdAndCityId",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(NeighborhoodServiceServer).CheckExistByProvinceIdAndCityId(ctx, req.(*RequestCheckExistByProvinceIdAndCityId))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _NeighborhoodService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "neighborhood.neighborhoodService",
 	HandlerType: (*NeighborhoodServiceServer)(nil),
@@ -490,6 +665,10 @@ var _NeighborhoodService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Create",
 			Handler:    _NeighborhoodService_Create_Handler,
+		},
+		{
+			MethodName: "CheckExistByProvinceIdAndCityId",
+			Handler:    _NeighborhoodService_CheckExistByProvinceIdAndCityId_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
